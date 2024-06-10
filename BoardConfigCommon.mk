cfed7edcbcefa01c64008f744d93b7749cce7535
Bruno Martins <bgcngm@gmail.com>
2024-06-10 19:51:18 +0530

    sm8650-common: Migrate to common libqti-perfd-client and power-libperfmgr
    Change-Id: I7fb7d253950a07baa85527b81d758191a86ea749
    Signed-off-by: inferno0230 <mail@inferno0230.in>
    Signed-off-by: grep <grepfox@tutamail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,6 +152,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
