6041ca752358df5e5d67ee2940ae666a30d8d2d1
grep <grepfox@tutamail.com>
2025-09-07 00:54:56 +0530

    sm8650-common: Declare vendor.qti.qegahal
    Change-Id: I3902a5fd0ab46f661aa9e1776c8d6d92dccc3607

--- common.mk
+++ common.mk
@@ -444,7 +444,8 @@ PRODUCT_COPY_FILES += \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    device/oneplus/sm8650-common/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/oneplus/sm8650-common/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
