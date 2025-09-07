61eb5761bcd269ea28e38c9db48f66c324d3838f
grep <grepfox@tutamail.com>
2025-09-07 12:45:45 +0530

    sm8650-common: Decommonise fingerprint service
    Change-Id: I256ee3251b247bb1a4c20a681f4f7dfca242ddf4

--- extract-files.py
+++ extract-files.py
@@ -91,8 +91,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff': blob_fixup()
-        .add_needed('libshims_aidl_fingerprint_v3.oplus.so'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so')
         .remove_needed('android.hardware.graphics.common-V4-ndk.so'),
